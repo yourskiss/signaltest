b1b1a917ba3ad17f5115fb87079569e2ea8ef5c9
yourskiss <ksingh1817@gmail.com>
2024-06-13 12:16:40 +0530

    final

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -52,8 +52,8 @@ onBackgroundMessage(messaging, (payload) => {
 */
 
 
-importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 
 firebase.initializeApp({
