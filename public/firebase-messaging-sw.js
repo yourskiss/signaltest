b56424f87579c373be42e8a4c8bfd62b69b48abb
yourskiss <ksingh1817@gmail.com>
2024-06-15 23:58:39 +0530

    final

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js');
 importScripts('/service-worker.js')
 
 firebase.initializeApp({
