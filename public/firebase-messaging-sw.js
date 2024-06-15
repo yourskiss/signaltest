5e5e8797bc2d5007e66fec155459390ea1cf52b0
yourskiss <ksingh1817@gmail.com>
2024-06-16 00:02:31 +0530

    final

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-// importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-messaging-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-app-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-messaging-compat.js');
 
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js');
 importScripts('/service-worker.js')
 
 firebase.initializeApp({
